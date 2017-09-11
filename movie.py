1a6c5fadb09e17974311594e9eccdc7580ec6300
Jackson Nabors <fjnabors@go.olemiss.edu>
2017-09-11 12:01:02 -0500

    pep8 guidelines

--- movie.py
+++ movie.py
@@ -1,5 +1,5 @@
-
 import webbrowser
+
 
 class Movie():
 
@@ -9,10 +9,13 @@ class Movie():
 
     def __init__(self, movie_title, movie_storyline, poster_image,
     trailer_youtube):
+        """Initialize title, storyline, poster_image, and youtube trailer url
+        attributes"""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """Opens webbrowser to display movie posters and trailers"""
         webbrowser.open(self.trailer_youtube_url)
